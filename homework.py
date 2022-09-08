4005de187f30ca45b53b71fdb59b74547302b293
GlebPogod1n <gleb1915526@gmail.com>
2022-09-08 23:06:21 +0300

    Sprint2

--- homework.py
+++ homework.py
@@ -13,11 +13,11 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> None:
-        return(f'Тип тренировки: {self.training_type}; '
-               f'Длительность: {self.duration:.3f} ч.; '
-               f'Дистанция: {self.distance:.3f} км; '
-               f'Ср. скорость: {self.speed:.3f} км/ч; '
-               f'Потрачено ккал: {self.calories:.3f}.')
+        return (f'Тип тренировки: {self.training_type}; '
+                f'Длительность: {self.duration:.3f} ч.; '
+                f'Дистанция: {self.distance:.3f} км; '
+                f'Ср. скорость: {self.speed:.3f} км/ч; '
+                f'Потрачено ккал: {self.calories:.3f}.')
 
 
 class Training:
